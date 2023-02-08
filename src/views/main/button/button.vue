ce7bf35de1f18a1187c041fbb0f2af6e1b5aebeb
janghoK <rlawkdgh0617@gmail.com>
2023-02-08 17:53:45 +0900

    implement button group section

---
+++ src/views/main/button/button.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="button-group">
+      <button class="mint-color">4컷 사진 찍기</button>
+      <button class="pink-color">4컷 사진 만들기</button>
+      <button class="kakao-color">친구들에게 공유하기</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+@Options({
+  components: {},
+})
+export default class ButtonSection extends Vue {}
+</script>
+
+<style lang="less">
+@import "./button.less";
+</style>
