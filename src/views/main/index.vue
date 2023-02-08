7882c6d12c14679b23918d122f9f26569cdf015f
janghoK <rlawkdgh0617@gmail.com>
2023-02-08 17:54:05 +0900

    add HeroView component

---
+++ src/views/main/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <HeroSection />
+  <ButtonSection />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HeroSection from "./hero/hero.vue";
+import ButtonSection from "./button/button.vue";
+
+@Options({
+  components: { HeroSection, ButtonSection },
+})
+export default class HeroView extends Vue {}
+</script>
